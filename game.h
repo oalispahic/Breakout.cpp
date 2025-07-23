b2fc082bf96e43e528d08a55db3aa2636f195b27
Omar Alispahic <omaralispahic@Omars-MacBook-Pro.local>
2025-07-23 18:35:25 +0200

    Added rendering and logic for moving

--- game.h
+++ game.h
@@ -1,7 +1,99 @@
-#ifndef BREAKOUT___LIBRARY_H
-#define BREAKOUT___LIBRARY_H
+#ifndef BREAKOUT___GAME_H
+#define BREAKOUT___GAME_H
+
+#include "objects.h"
+#include <termios.h>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+
+class Game {
+    int mapX, mapY;
+    Paddle player;
+    bool validPaddlePos();
+public:
+    Game(int mapX, int mapY) : mapX(mapX), mapY(mapY), player(7,17,5) {
+    }
+
+    static void clearScreen();
+
+    void setBufferedInput(bool enable);
+
+    void render();
+
+    static void delay(int time);
+
+    void commands(char input);
+};
 
 
+void Game::clearScreen() {
+    std::cout << "\033[2J\033[H" << std::flush;
+}
+
+void Game::setBufferedInput(bool enable) {
+    static bool initialized = false;
+    static struct termios old;
+    struct termios newt;
+
+    if (!initialized) {
+        tcgetattr(STDIN_FILENO, &old);
+        initialized = true;
+    }
+
+    newt = old;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    if (enable) newt = old;
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+}
+
+void Game::render() {
+    bool paddleRendered = false;
+    Game::clearScreen();
+    for (int i = 0; i < mapX; i++) {
+        int ignoreCounter = 0;
+        for (int j = 0; j < mapY; j++) {
+            if (i == 0 || i == mapX - 1 || j == 0 || j == mapY - 1) {
+                std::cout << "* ";
+                continue;
+            }
 
 
-#endif //BREAKOUT___LIBRARY_H
+            if (player.pointArray(0).y == i && !paddleRendered && player.pointArray(0).x == j) {
+                for (int k = 0; k < player.paddleSize(); k++) {
+                    std::cout << "--";
+                }
+
+                paddleRendered = true;
+
+            }
+            if (ignoreCounter < player.paddleSize() && paddleRendered) {
+                ignoreCounter++;
+            } else std::cout << "  ";
+
+        }
+        paddleRendered = false;
+        std::cout << std::endl;
+    }
+}
+
+void Game::delay(int time) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(time));
+}
+
+void Game::commands(char input) {
+switch(input){
+    case 'd':{
+        player.updateX(1);
+        break;
+    }
+    case 'a':{
+        player.updateX(-1);
+        break;
+    }
+}
+}
+
+#endif //BREAKOUT___GAME_H
